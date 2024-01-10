2447ef33abbf21fe5d2af56889f8b5a31443fd5d
Francis Acquah <cylic24darers@gmail.com>
2024-01-10 01:58:31 +0000

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include "list.h"
+/**
+ * check_cycle - a function that checks if a singly
+ * linked list has a cycle in it
+ * @list: struct
+ * Return: 0 if no cycle or 1 if cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	retrun 0
+}
