dcb2f3d08e61d8fe26b827e6e6b75d681d198463
Francis Acquah <cylic24darers@gmail.com>
2024-02-01 23:28:39 +0000

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,5 +3,4 @@ for num in range(0, 100):
     if num in [99]:
         print("{}".format(num))
     else:
-        print("{:02d}, ".format(num),end="")
-    
+        print("{:02d}, ".format(num), end="")
